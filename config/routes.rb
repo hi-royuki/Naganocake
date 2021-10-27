57e2d741200910ad1fc36ccece11f9cc4cfc319a
EC2 Default User <ec2-user@ip-172-31-53-94.ec2.internal>
2021-10-27 07:13:36 +0000

    [add]管理者作成中

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :admin do
+    get 'items/index'
+    get 'items/new'
+    get 'items/show'
+    get 'items/edit'
+  end
+  devise_for :customers, controllers: {
+  sessions:      'customers/sessions',
+  passwords:     'customers/passwords',
+  registrations: 'customers/registrations'
+}
+  devise_for :admins, controllers: {
+  sessions:      'admins/sessions'
+}
+
 end
