893be33d5619e2d4eee920404b1962ded86d92fb
hi-royuki <hiro.0621.ito3835@gmail.com>
2021-10-28 15:32:06 +0000

    [fix]

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -10,7 +10,7 @@ class Admin::ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    
+
     if @item.save
       redirect_to admin_items_path
       # もし保存されなかったらitem/newへ
@@ -22,20 +22,23 @@ class Admin::ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
-  
+
   def edit
     @item = Item.find(params[:id])
   end
 
   def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to admin_item_path(@item.id)
   end
 
-  
+
  private
- 
+
   def item_params
     params.require(:item).permit(:image, :name, :introduction, :price, :genre_id, :is_active)
   end
- 
- 
+
+
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'b0534ed633e8ed5c54458115c64f8f0c88ebb08fd349b90b8db13702c494001d7a5fc8673c2611a86363229d916452663e36f33fa9ef3825c2b3f4e76f8d4b20'
