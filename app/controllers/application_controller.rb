2ad310d45947dcae9848c87f816b2d9c54cf2cb6
hi-royuki <hiro.0621.ito3835@gmail.com>
2021-10-29 08:21:20 +0000

    [fix]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,18 @@
 class ApplicationController < ActionController::Base
-   before_action :authenticate_customer!,except: [:top, :about]
+  
    before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number])
+  end
+
+  def after_sign_in_path_for(resource)
+    admin_items_path
+  end
+
+  def after_sign_out_path_for(resource)
+    new_admin_session_path
   end
 end
--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,9 +1,10 @@
 class Public::CustomersController < ApplicationController
   def show
-    
+    @customer = current_customer
   end
 
   def edit
+    @customer = current_customer
   end
   
   def update
@@ -14,4 +15,12 @@ class Public::CustomersController < ApplicationController
   
   def withdraw
   end
+  
+   private
+
+   def customer_params
+     	params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :phone_number, :email)
+   end
+
+
 end
