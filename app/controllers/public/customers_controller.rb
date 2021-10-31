b9ce2fe8e1bdf80de9f845b134d8a23f6e6f6da2
hi-royuki <hiro.0621.ito3835@gmail.com>
2021-10-31 13:23:32 +0000

    [fix]退会機能記述

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -6,22 +6,32 @@ class Public::CustomersController < ApplicationController
   def edit
     @customer = current_customer
   end
-  
+
   def update
-    
+    @customer = current_customer
+    if @customer.update(customer_params)
+      redirect_to customers_path(@customer)
+    else
+      render :edit
+    end
   end
 
   def unsubscribe
+     @customer = current_customer
   end
-  
+
   def withdraw
+    @customer = current_customer
+    @customer.update(is_active: false)# //is_deletedをtrueへ
+    
+    reset_session # ログアウトさせる
+    flash[:notice] = "ありがとうございました。又のご利用を心よりお待ちしております。"
+    redirect_to root_path
   end
-  
+
    private
 
    def customer_params
      	params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :phone_number, :email)
    end
-
-
 end
