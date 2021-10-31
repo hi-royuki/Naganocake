486e343e312869e662f64a28608fc1feb88bf3f3
hi-royuki <hiro.0621.ito3835@gmail.com>
2021-10-31 13:21:56 +0000

    [fix]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,22 @@
 class ApplicationController < ActionController::Base
+   
   
    before_action :configure_permitted_parameters, if: :devise_controller?
+   
+    
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number])
+  end
+  
+  def after_sign_out_path_for(resources)
+    if resources == :admin
+      new_admin_session_path
+    else
+      customers_sign_in_path
+    end
   end
 
   
