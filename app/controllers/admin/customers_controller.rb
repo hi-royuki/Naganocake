b8a09cf1c8c8a7d60be4dd71b3d5624f7549d07c
hi-royuki <hiro.0621.ito3835@gmail.com>
2021-10-31 01:02:31 +0000

    [fix]顧客　の詳細ページ（未　リンクボタン）

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,15 +1,31 @@
 class Admin::CustomersController < ApplicationController
-  
+
+# 管理者ページの顧客のコントローラ
+
   def index
+    @customers = Customer.all
   end
-  
+
   def show
+    # 顧客の詳細
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    # 顧客の会員情報編集
+    @customer = Customer.find(params[:id])
   end
-  
+
   def update
   end
-  
+
+  private
+
+  private
+
+  def customer_params
+  	params.require(:customer).permit(:email, :last_name, :first_name, :last_name_kana, :postal_code,:address, :phone_number, :is_active)
+  end
+
+
 end
