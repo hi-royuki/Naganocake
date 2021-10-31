f5d5b2bd319c1e42b325ca206e1be3a523e390d0
hi-royuki <hiro.0621.ito3835@gmail.com>
2021-10-31 04:26:23 +0000

    [fix]バリエーション追加→新規投稿はできるが，から入力するとエラーになる

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,12 +1,12 @@
 class Admin::GenresController < ApplicationController
-  
-  
+
+
   def index
     # index.htmlのなかに投稿機能
     @genre = Genre.new
     @genres = Genre.all
   end
-  
+
   def create
     @genre = Genre.new(genre_params)
     if @genre.save
@@ -19,13 +19,13 @@ class Admin::GenresController < ApplicationController
   def edit
     @genre = Genre.find(params[:id])
   end
-  
+
   def update
     @genre = Genre.find(params[:id])
     @genre.update(genre_params)
       redirect_to admin_genres_path
-  end 
-  
+  end
+
   private
    def genre_params
      params.require(:genre).permit(:name)
