ae7e55b806452b80e2e653f90b1204bef4bc1ec5
hi-royuki <hiro.0621.ito3835@gmail.com>
2021-11-02 12:13:43 +0000

    [fix]ログインできない現象解決

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,10 +7,17 @@ class Customer < ApplicationRecord
          has_many :orders, dependent: :destroy
          has_many :addresses, dependent: :destroy
          has_many :cart_items, dependent: :destroy
-         
-         
+
+
 
         # 必須項目
          validates :last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, presence: true
 
+        #退会ユーザーはログインできなくする
+        # ログインする時に退会済み(is_active==true)のユーザーを弾くためのメソッド
+        
+        # 
+        # def active_for_authentication?
+        #     super && (self.is_active == false)
+        # end
 end
