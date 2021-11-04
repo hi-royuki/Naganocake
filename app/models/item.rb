fcf1889928cbfe85428fb6722dccf3b513f8568f
hi-royuki <hiro.0621.ito3835@gmail.com>
2021-11-04 03:35:45 +0000

    [fix]消費税を求めるメソッド記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,13 @@
 class Item < ApplicationRecord
 
+  belongs_to :genre
+
   attachment :image
 
   has_many :cart_items
   has_many :order_details
 
-  belongs_to :genre
+
 
  enum is_active: { on_sale: 0, off_sale: 1 }
 
@@ -13,5 +15,11 @@ class Item < ApplicationRecord
   validates :name, presence: true
   validates :introduction, presence: true
   validates :price, presence: true
+  
+  
+# 消費税を求めるメソッド
+  def with_tax_price
+    (price * 1.1).floor
+  end
 
 end
