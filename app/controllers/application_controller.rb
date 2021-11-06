2d75e54489ba0cb1ed156cbcad22019aaf71f29e
hi-royuki <hiro.0621.ito3835@gmail.com>
2021-11-06 14:50:00 +0000

    [fix]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,24 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
 
+  def after_sign_in_path_for(resource)
+    if current_customer
+     root_path
+
+    elsif Admin
+     admin_root_path
+    end
+  end
+
+    def after_sign_out_path_for(resource_or_scope)
+      if resource_or_scope == :customer
+        root_path
+      elsif resource_or_scope == :admin
+        admin_session_path
+      end
+    end
+
+
 
   protected
 
@@ -10,13 +28,13 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number])
   end
 
-  def after_sign_out_path_for(resource)
-    new_admin_session_path
-  end
+  # def after_sign_out_path_for(resource)
+  #   new_admin_session_path
+  # end
 
-  def after_sign_up_path_for(resource)
-    admin_items_path
-  end
+  # def after_sign_up_path_for(resource)
+  #   admin_items_path
+  # end
 
 
 end
--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,5 +1,7 @@
 class Public::OrdersController < ApplicationController
 
+ before_action  :check_cart, only: [:new]
+
 
 
   def new
@@ -10,7 +12,7 @@ class Public::OrdersController < ApplicationController
   def confirm
     @cart_items = current_customer.cart_items
     @order = Order.new(order_params)
-
+    @order.customer_id = current_customer.id
     if params[:order][:address_id] == "1"
        @order.postal_code = current_customer.postal_code
        @order.address = current_customer.address
@@ -34,6 +36,7 @@ class Public::OrdersController < ApplicationController
   def create
     @cart_items = current_customer.cart_items
     @order = current_customer.orders.new(order_params)
+    total_payment = 0
     @cart_items.each do |cart_item|
       order_detail = OrderDetail.new
       order_detail.item_id = cart_item.item_id
@@ -41,20 +44,33 @@ class Public::OrdersController < ApplicationController
       order_detail.amount = cart_item.amount
       order_detail.order_id = @order.id
       order_detail.save
+      total_payment = cart_item.sum_of_price() + total_payment
     end
+
+     @order.total_payment = total_payment
+
+     @order.save
      redirect_to  complete_orders_path
      @cart_items.destroy_all
   end
 
   def index
-    @orders = current_customer.orders
+    @orders = Order.all
   end
 
   def show
+     @order = Order.find(params[:id])
+     @order_items = @order.order_items
   end
 
   private
 def order_params
-    params.require(:order).permit(:payment_method, :postal_code, :address, :name, :customer_id)
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name, :customer_id, :shipping_cost)
 end
+
+ def check_cart
+    if current_customer.cart_items.blank?
+      redirect_to cart_items_path
+    end
+ end
 end
